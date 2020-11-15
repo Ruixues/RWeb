ea591d1aa9713c4f257084ab9adf5f598f22bddf
Dorbmon <1263592223@qq.com>
2020-11-15 16:40:49 +0800

    增加Write

--- context.go
+++ context.go
@@ -13,6 +13,9 @@ type Context struct {
 func (z *Context) FormValue (key string) []byte {
 	return z.RawCtx.FormValue(key)
 }
+func (z *Context) Write (data []byte) {
+	z.RawCtx.Write(data)
+}
 var contextPool = &sync.Pool{
 	New: func() interface{} {
 		return new(Context)
